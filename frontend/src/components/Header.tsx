582af785f92738cef5000fe70864a18397f8b967
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2026-04-21 19:28:17 -0400

    file structure + updates

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+// practice React component (with PascalCasing) -- rendering h1 element
+function Header(){
+    const name = 'Inaya Rajwani';
+    return <h1>Hello {name}</h1>
+}
+
+export default Header;
